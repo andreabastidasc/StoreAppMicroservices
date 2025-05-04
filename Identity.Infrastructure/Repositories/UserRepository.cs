d0d0e754200b5ecf1ad901e9717be1c4c3ca0291
andreabastidasc <andrea.bcolina@gmail.com>
2025-05-04 20:18:29 -0300

    add user repository

---
+++ Identity.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using Identity.Domain.Entities;
+using Identity.Domain.Interfaces;
+using Identity.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using BCrypt.Net;
+
+namespace Identity.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserDbContext _context;
+
+        public UserRepository(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(UserEntity user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<UserEntity?> GetByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<bool> ValidateUserAsync(string email, string password)
+        {
+            var user = await GetByEmailAsync(email);
+            if (user == null) return false;
+
+            return BCrypt.Net.BCrypt.Verify(password, user.PasswordHash);
+        }
+    }
+}
