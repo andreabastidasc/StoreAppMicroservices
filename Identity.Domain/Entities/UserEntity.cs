96c64bc14a79a8d060d4df354106a05176bdd6f5
andreabastidasc <andrea.bcolina@gmail.com>
2025-05-04 19:50:57 -0300

    add user entity

---
+++ Identity.Domain/Entities/UserEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Domain.Entities
+{
+    public class UserEntity
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public Guid CustomerId { get; set; }  // Relación al Customer
+    }
+}
---
+++ Identity.Infrastructure/Data/UserDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Identity.Domain.Entities;
+
+namespace Identity.Infrastructure.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+
+        public DbSet<UserEntity> Users => Set<UserEntity>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
