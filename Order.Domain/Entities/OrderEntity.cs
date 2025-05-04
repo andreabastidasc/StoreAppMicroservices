d60c37e6568d557475b41ca0a62cf88213ba6fd4
andreabastidasc <andrea.bcolina@gmail.com>
2025-05-04 16:46:37 -0300

    create order entity

---
+++ Order.Domain/Entities/OrderEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Order.Domain.Entities
+{
+    public class OrderEntity
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal Total { get; set; }
+    }
+}
