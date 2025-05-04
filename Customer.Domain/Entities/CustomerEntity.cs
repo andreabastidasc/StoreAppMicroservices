e4b78e1a6ef374a117b02600fdfff29b0d569a19
andreabastidasc <andrea.bcolina@gmail.com>
2025-05-04 14:24:41 -0300

    add customer entity

---
+++ Customer.Domain/Entities/CustomerEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer.Domain.Entities
+{
+    public class CustomerEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+    }
+}
+
