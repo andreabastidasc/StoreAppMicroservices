6294525a376920dc481ad65255204b3c96f8c80f
andreabastidasc <andrea.bcolina@gmail.com>
2025-05-04 19:52:57 -0300

    add repository interface

---
+++ Identity.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Identity.Domain.Entities;
+
+namespace Identity.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task AddAsync(UserEntity user);
+        Task<UserEntity?> GetByEmailAsync(string email);
+        Task<bool> ValidateUserAsync(string email, string password);
+    }
+}
