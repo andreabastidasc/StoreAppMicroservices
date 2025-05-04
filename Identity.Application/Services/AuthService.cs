0a1dc72f8af292cea9e82432e69fb7bf42ab6f6d
andreabastidasc <andrea.bcolina@gmail.com>
2025-05-04 20:30:38 -0300

    add auth service

---
+++ Identity.Application/Services/AuthService.cs
@@ -0,0 +1,42 @@
+﻿using Identity.Application.DTOs;
+using Identity.Domain.Entities;
+using Identity.Domain.Interfaces;
+using BCrypt.Net;
+
+namespace Identity.Application.Services
+{
+    public class AuthService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AuthService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<bool> RegisterAsync(UserDto dto)
+        {
+            var existingUser = await _userRepository.GetByEmailAsync(dto.Email);
+            if (existingUser != null)
+                return false;
+
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(dto.Password);
+
+            var user = new UserEntity
+            {
+                Id = Guid.NewGuid(),
+                Email = dto.Email,
+                PasswordHash = hashedPassword,
+                Name = dto.Name
+            };
+
+            await _userRepository.AddAsync(user);
+            return true;
+        }
+
+        public async Task<bool> LoginAsync(UserDto dto)
+        {
+            return await _userRepository.ValidateUserAsync(dto.Email, dto.Password);
+        }
+    }
+}
